1d0f4efec4b41d5dd1c2eae36de111c9b33136d9
Sean Stoops <brutimus@gmail.com>
2019-10-11 17:16:41 -0700

    Converting to new style middleware.

--- amp_tools/middleware.py
+++ amp_tools/middleware.py
@@ -1,28 +1,19 @@
-
 from amp_tools.settings import settings
 from amp_tools import set_amp_detect
 
 
 class AMPDetectionMiddleware(object):
-
     def __init__(self, get_response=None):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-
-        response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
-        return response
-
-    def process_request(self, request):
         if settings.AMP_TOOLS_GET_PARAMETER in request.GET:
-            if request.GET[settings.AMP_TOOLS_GET_PARAMETER] == settings.AMP_TOOLS_GET_VALUE:
+            if (
+                request.GET[settings.AMP_TOOLS_GET_PARAMETER]
+                == settings.AMP_TOOLS_GET_VALUE
+            ):
                 set_amp_detect(is_amp_detect=True, request=request)
         else:
             set_amp_detect(is_amp_detect=False, request=request)
+
+        return self.get_response(request)
