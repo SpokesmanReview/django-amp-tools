f21f28b0ee0b808d058796af3bdbf59f9b0c5730
Sean Stoops <brutimus@gmail.com>
2021-01-21 15:06:07 -0800

    It appears merging in the changes from another origin broke the middleware. It kind of looks like other forks are not even using the middleware and thus it's completely non-functional. Fixing back to the way I had it previously.

--- amp_tools/middleware.py
+++ amp_tools/middleware.py
@@ -5,23 +5,10 @@ from amp_tools import set_amp_detect
 
 
 class AMPDetectionMiddleware(object):
-
     def __init__(self, get_response=None):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-
-        response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
-        return response
-
-    def process_request(self, request):
         if settings.AMP_TOOLS_GET_PARAMETER in request.GET:
             if request.GET[settings.AMP_TOOLS_GET_PARAMETER] == settings.AMP_TOOLS_GET_VALUE:
                 if settings.AMP_TOOLS_ACTIVE_URLS:
@@ -36,3 +23,5 @@ class AMPDetectionMiddleware(object):
                     set_amp_detect(is_amp_detect=True, request=request)
         else:
             set_amp_detect(is_amp_detect=False, request=request)
+
+        return self.get_response(request)
